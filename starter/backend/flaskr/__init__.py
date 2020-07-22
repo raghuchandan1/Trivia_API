cd5f8fd3bb3e66904da82a3beb979bfe6773f0a2
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-07-22 22:35:23 +0530

    fix: Minor fix in response for adding a new question

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -142,7 +142,7 @@ def create_app(test_config=None):
 
                 return jsonify({
                     'success': True,
-                    'created': question.id,
+                    'created': question.id
                 })
 
             except:
