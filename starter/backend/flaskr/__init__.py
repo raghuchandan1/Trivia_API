58d3a64381d273558a61b547c172bb523a968822
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-07-22 13:40:55 +0530

    feat: Added route decorator for playing a quiz at /quizzes

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -126,31 +126,49 @@ def create_app(test_config=None):
     @app.route("/questions", methods=['POST'])
     def add_question():
         body = request.get_json()
+        if not ('searchTerm' in body):
+            if not ('question' in body and 'answer' in body and 'difficulty' in body and 'category' in body):
+                abort(422)
 
-        if not ('question' in body and 'answer' in body and 'difficulty' in body and 'category' in body):
-            abort(422)
+            new_question = body.get('question')
+            new_answer = body.get('answer')
+            new_difficulty = body.get('difficulty')
+            new_category = body.get('category')
 
-        new_question = body.get('question')
-        new_answer = body.get('answer')
-        new_difficulty = body.get('difficulty')
-        new_category = body.get('category')
+            try:
+                question = Question(question=new_question, answer=new_answer,
+                                    difficulty=new_difficulty, category=new_category)
+                question.insert()
 
-        try:
-            question = Question(question=new_question, answer=new_answer,
-                                difficulty=new_difficulty, category=new_category)
-            question.insert()
+                return jsonify({
+                    'success': True,
+                    'created': question.id,
+                })
 
-            return jsonify({
-                'success': True,
-                'created': question.id,
-            })
+            except:
+                abort(422)
+        searchTerm = body.get('searchTerm')
+        selection = Question.query.filter(Question.question.ilike('%{}%'.format(searchTerm))).all()
+        current_questions = paginate_questions(request, selection)
 
-        except:
-            abort(422)
+        # categories = Category.query.order_by(Category.type).all()
+        categories = Category.query.order_by(Category.type).all()
+        # formatted_categories = [category.format() for category in categories]
 
+        if len(current_questions) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'questions': current_questions,
+            'total_questions': len(selection),
+            'categories': {category.id: category.type for category in categories},
+            # 'categories': formatted_categories,
+            'current_category': None
+        })
 
     '''
-  @TODO: 
+  @DONE: 
   Create a POST endpoint to get questions based on a search term. 
   It should return any questions for whom the search term 
   is a substring of the question. 
@@ -159,9 +177,9 @@ def create_app(test_config=None):
   only question that include that string within their question. 
   Try using the word "title" to start. 
   '''
-
+    #@app.route('/questions',methods=['POST])
     '''
-  @TODO: 
+  @DONE: 
   Create a GET endpoint to get questions based on category. 
 
   TEST: In the "List" tab / main screen, clicking on one of the 
@@ -169,6 +187,22 @@ def create_app(test_config=None):
   category to be shown. 
   '''
 
+    @app.route('/categories/<int:category_id>/questions', methods=['GET'])
+    def retrieve_questions_by_category(category_id):
+
+        try:
+            questions = Question.query.filter(
+                Question.category == str(category_id)).all()
+            categories = Category.query.order_by(Category.type).all()
+            return jsonify({
+                'success': True,
+                'questions': [question.format() for question in questions],
+                'total_questions': len(questions),
+                'categories': {category.id: category.type for category in categories},
+                'current_category': category_id
+            })
+        except:
+            abort(404)
     '''
   @TODO: 
   Create a POST endpoint to get questions to play the quiz. 
@@ -180,6 +214,33 @@ def create_app(test_config=None):
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not. 
   '''
+    @app.route('/quizzes',methods=['POST'])
+    def play_quiz():
+        try:
+            body = request.get_json()
+
+            if not ('quiz_category' in body and 'previous_questions' in body):
+                abort(422)
+
+            category = body.get('quiz_category')
+            previous_questions = body.get('previous_questions')
+
+            if category['type'] == 'click':
+                available_questions = Question.query.filter(
+                    Question.id.notin_(previous_questions)).all()
+            else:
+                available_questions = Question.query.filter_by(
+                    category=category['id']).filter(Question.id.notin_(previous_questions)).all()
+
+            new_question = available_questions[random.randrange(
+                0, len(available_questions))].format() if len(available_questions) > 0 else None
+
+            return jsonify({
+                'success': True,
+                'question': new_question
+            })
+        except:
+            abort(422)
 
     '''
   @TODO: 
